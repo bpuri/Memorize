06d29234a76128744f704f0c25ce10450b807da8
bpuri <bhvn.puri@gmail.com>
2025-04-03 20:25:58 -0700

    Arraified Card with for each

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,14 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    
+    let emojis: Array<String> = ["👻", "🎃", "👿", "🕷️", "🕷️"]
+
     var body: some View {
         HStack{
-            CardView(isFaceUp: true)
-            CardView()
-            CardView(isFaceUp: true)
-            CardView()
-            CardView()
+            ForEach(emojis.indices, id: \.self){ index in
+                CardView(content: emojis[index])
+            }
         }
         .foregroundColor(.orange)
         .padding()
@@ -24,7 +23,8 @@ struct ContentView: View {
 
 struct CardView: View{
     
-    @State var isFaceUp = false
+    let content: String
+    @State var isFaceUp = true
     
     var body: some View{
         ZStack{
@@ -33,7 +33,7 @@ struct CardView: View{
             if isFaceUp{
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
-                Text("👻").font(.largeTitle)
+                Text(content).font(.largeTitle)
             } else {
                 base.fill()
             }
